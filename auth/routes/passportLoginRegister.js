98bba7055de606629649e2ca0171a283c5b7e114
Mohammad Eidimorad <m.eidimorad@gmail.com>
2020-11-13 01:40:42 +0330

    undo login callback method

--- auth/routes/passportLoginRegister.js
+++ auth/routes/passportLoginRegister.js
@@ -13,7 +13,36 @@ router.post('/registerBasic', async (req, res) => {
     await registerUser(req, res, "basic");
 });
 
-router.post('/login', newFunction());
+router.post('/login', (req, res) => {
+    passport.authenticate(
+        'local',
+        { session: false },
+        (error, user) => {
+            if (error || !user) {
+                res.status(401).json({ error });
+                return;
+            }
+            /** This is what ends up in our JWT */
+            const payload = {
+                email: user.email,
+                role: user.role,
+                expires: Date.now() + parseInt(120000),
+            };
+            /** assigns payload to req.user */
+            req.login(payload, { session: false }, (error) => {
+                if (error) {
+                    res.status(401).send({ error });
+                    return;
+                }
+                /** generate a signed json web token and return it in the response */
+                const token = jwt.sign(JSON.stringify(payload), "ABCDEFG");
+                /** assign our jwt to the cookie */
+                res.cookie('jwt', token, { httpOnly: true, secure: true });
+                res.status(200).send({ token });
+            });
+        },
+    )(req, res);
+});
 
 
 
@@ -41,36 +70,3 @@ registerUser = async (req, res, role) => {
         });
     }
 }
-function newFunction() {
-    return (req, res) => {
-        passport.authenticate(
-            'local',
-            { session: false },
-            (error, user) => {
-                if (error || !user) {
-                    res.status(401).json({ error });
-                    return;
-                }
-                /** This is what ends up in our JWT */
-                const payload = {
-                    email: user.email,
-                    role: user.role,
-                    expires: Date.now() + parseInt(120000),
-                };
-                /** assigns payload to req.user */
-                req.login(payload, { session: false }, (error) => {
-                    if (error) {
-                        res.status(401).send({ error });
-                        return;
-                    }
-                    /** generate a signed json web token and return it in the response */
-                    const token = jwt.sign(JSON.stringify(payload), "ABCDEFG");
-                    /** assign our jwt to the cookie */
-                    res.cookie('jwt', token, { httpOnly: true, secure: true });
-                    res.status(200).send({ token });
-                });
-            }
-        )(req, res);
-    };
-}
-
